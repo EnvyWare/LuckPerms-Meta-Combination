9512bd44bce6fa6ade7f61814c9890358a570d66
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-06-30 15:44:54 +0100

    Feat: registered listener and loaded config in main class

--- src/main/java/com/envyful/lp/meta/combine/LPMetaCombine.java
+++ src/main/java/com/envyful/lp/meta/combine/LPMetaCombine.java
@@ -1,19 +1,39 @@
 package com.envyful.lp.meta.combine;
 
+import com.envyful.api.config.yaml.YamlConfigFactory;
+import com.envyful.lp.meta.combine.config.MetaCombineConfig;
+import com.envyful.lp.meta.combine.listener.PlayerJoinListener;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+import java.io.IOException;
 
 @Mod(
         modid = "lpmetacombine",
         name = "LPMetaCombine",
         version = "0.0.1",
-        acceptableRemoteVersions = "*",
-        dependencies = "required-after:luckperms;"
+        acceptableRemoteVersions = "*"
 )
 public class LPMetaCombine {
 
+    private MetaCombineConfig config;
+
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
+        this.loadConfig();
+    }
 
+    @Mod.EventHandler
+    public void onServerStart(FMLServerStartingEvent event) {
+        new PlayerJoinListener(this.config);
+    }
+
+    private void loadConfig() {
+        try {
+            this.config = YamlConfigFactory.getInstance(MetaCombineConfig.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
